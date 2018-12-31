e506cdb6ac9d607475560099f6a31f52d16cb0ff
Tim Harper <timcharper@gmail.com>
2018-12-31 15:27:08 -0700

    Handle tear-down better. Get rid of null warnings.

--- addons/binding/org.openhab.binding.geothunk/src/main/java/org/openhab/binding/geothunk/internal/GeoThunkHandler.java
+++ addons/binding/org.openhab.binding.geothunk/src/main/java/org/openhab/binding/geothunk/internal/GeoThunkHandler.java
@@ -14,8 +14,17 @@ package org.openhab.binding.geothunk.internal;
 
 import static org.openhab.binding.geothunk.internal.GeoThunkBindingConstants.*;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Optional;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import javax.measure.quantity.Temperature;
+
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.eclipse.jdt.annotation.NonNullByDefault;
@@ -23,24 +32,21 @@ import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.smarthome.core.library.types.DecimalType;
 import org.eclipse.smarthome.core.library.types.QuantityType;
 import org.eclipse.smarthome.core.library.types.StringType;
-import org.eclipse.smarthome.core.thing.*;
-import org.eclipse.smarthome.core.types.State;
-import org.eclipse.smarthome.core.types.UnDefType;
-import org.openhab.binding.geothunk.internal.GeoThunkConfiguration;
+import org.eclipse.smarthome.core.thing.Channel;
+import org.eclipse.smarthome.core.thing.ChannelUID;
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.thing.ThingStatus;
+import org.eclipse.smarthome.core.thing.ThingStatusDetail;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
 import org.eclipse.smarthome.core.types.Command;
 import org.eclipse.smarthome.core.types.RefreshType;
+import org.eclipse.smarthome.core.types.State;
+import org.eclipse.smarthome.core.types.UnDefType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.measure.quantity.Temperature;
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 
 /**
  * The {@link GeoThunkHandler} is responsible for handling commands, which are
@@ -54,16 +60,11 @@ public class GeoThunkHandler extends BaseThingHandler {
     private static final int DEFAULT_REFRESH_PERIOD_SECONDS = 600; // 10 minutes
     private final Logger logger = LoggerFactory.getLogger(GeoThunkHandler.class);
 
-    @Nullable
-    private GeoThunkConfiguration config;
+    private Optional<ScheduledFuture<?>> refreshJob = Optional.empty();
 
-    private ScheduledFuture<?> refreshJob;
-
-    private GeoThunkJsonResponse csResponse;
+    private GeoThunkJsonResponse csResponse = new GeoThunkJsonResponse();
 
     private Gson gson;
-
-    private int retryCounter = 0;
 
     public GeoThunkHandler(Thing thing) {
         super(thing);
@@ -95,9 +96,7 @@ public class GeoThunkHandler extends BaseThingHandler {
             }
 
             State state = null;
-            if (value == null) {
-                state = UnDefType.UNDEF;
-            } else if (value instanceof QuantityType<?>) {
+            if (value instanceof QuantityType<?>) {
                 state = (QuantityType<?>) value;
             } else if (value instanceof BigDecimal) {
                 state = new DecimalType((BigDecimal) value);
@@ -110,7 +109,7 @@ public class GeoThunkHandler extends BaseThingHandler {
                         value.getClass().getSimpleName());
             }
             logger.debug("Update channel {} with state {} ({})", channelId, (state == null) ? "null" : state.toString(),
-                    (value == null) ? "null" : value.getClass().getSimpleName());
+                    value.getClass().getSimpleName());
 
             // Update the channel
             if (state != null) {
@@ -122,29 +121,26 @@ public class GeoThunkHandler extends BaseThingHandler {
     public static Object getValue(String channelId, GeoThunkJsonResponse data) throws Exception {
         String[] fields = StringUtils.split(channelId, "#");
 
-        if (data != null) {
-            switch (fields[0]) {
-                case PM1:
-                    return data.getPm1();
-                case PM25:
-                    return data.getPm2_5();
-                case PM10:
-                    return data.getPm10();
-                case TEMPERATURE:
-                    return new QuantityType<Temperature>(data.getTemperatureCelsius(), API_TEMPERATURE_UNIT);
-                case HUMIDITY:
-                    return new QuantityType<>(data.getHumidityPercent(), API_HUMIDITY_UNIT);
-            }
+        switch (fields[0]) {
+            case PM1:
+                return data.getPm1();
+            case PM25:
+                return data.getPm2_5();
+            case PM10:
+                return data.getPm10();
+            case TEMPERATURE:
+                return new QuantityType<Temperature>(data.getTemperatureCelsius(), API_TEMPERATURE_UNIT);
+            case HUMIDITY:
+                return new QuantityType<>(data.getHumidityPercent(), API_HUMIDITY_UNIT);
+            default:
+                return UnDefType.UNDEF;
         }
-
-        return null;
     }
-
 
     @Override
     public void initialize() {
         // logger.debug("Start initializing!");
-        config = getConfigAs(GeoThunkConfiguration.class);
+        GeoThunkConfiguration config = getConfigAs(GeoThunkConfiguration.class);
         logger.debug("config url = {}", config.url);
         logger.debug("config refreshSeconds = {}", config.refreshSeconds);
 
@@ -153,56 +149,70 @@ public class GeoThunkHandler extends BaseThingHandler {
             errorMsg = "Parameter 'url' is mandatory and must be configured";
         }
         if (errorMsg == null) {
+            String url = buildRequestURL(config.url);
+            int delay = (config.refreshSeconds != null) ? config.refreshSeconds : DEFAULT_REFRESH_PERIOD_SECONDS;
             updateStatus(ThingStatus.ONLINE);
-            startAutomaticRefresh();
+            startAutomaticRefresh(url, delay);
         } else {
             updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR, errorMsg);
         }
     }
 
+    @Override
+    public void dispose() {
+        logger.debug("Disposing of GeoThunk thing, stopping handler");
+        refreshJob = refreshJob.flatMap(j -> {
+            j.cancel(true);
+            return Optional.empty();
+        });
+    }
+
     /**
      * Start the job refreshing the Air Quality data
      */
-    private void startAutomaticRefresh() {
-        if (refreshJob == null || refreshJob.isCancelled()) {
+    private void startAutomaticRefresh(String url, int delaySeconds) {
+        if (!refreshJob.filter(j -> !j.isCancelled()).isPresent()) {
             Runnable runnable = () -> {
                 try {
                     // Request new air quality data to the aqicn.org service
-                    retryCounter = 0;
-                    csResponse = getGeoThunkData();
+                    GeoThunkJsonResponse response = getGeoThunkData(url);
 
-                    // Update all channels from the updated AQI data
-                    for (Channel channel : getThing().getChannels()) {
-                        updateChannel(channel.getUID().getId(), csResponse);
+                    if (response != null) {
+                        csResponse = response;
+                        // Update all channels from the updated AQI data
+                        for (Channel channel : getThing().getChannels()) {
+                            updateChannel(channel.getUID().getId(), response);
+                        }
                     }
                 } catch (Exception e) {
                     logger.error("Exception occurred during execution: {}", e.getMessage(), e);
                 }
             };
 
-            GeoThunkConfiguration config = getConfigAs(GeoThunkConfiguration.class);
-            int delay = (config.refreshSeconds != null) ? config.refreshSeconds : DEFAULT_REFRESH_PERIOD_SECONDS;
-            refreshJob = scheduler.scheduleWithFixedDelay(runnable, 0, delay, TimeUnit.SECONDS);
+            logger.debug("Scheduled handler for {} every {} seconds", url, delaySeconds);
+            refreshJob = Optional.of(scheduler.scheduleWithFixedDelay(runnable, 0, delaySeconds, TimeUnit.SECONDS));
         }
     }
 
-    private String buildRequestURL() {
-        String url = config.url;
-        if (!config.url.endsWith("/"))
-            url = url + "/";
+    private String buildRequestURL(String pBaseUrl) {
+        String baseUrl = pBaseUrl;
+        if (!baseUrl.endsWith("/")) {
+            baseUrl = baseUrl + "/";
+        }
 
-        return url + "stats";
+        return baseUrl + "stats";
     }
+
     /**
      * Request new air quality data to the aqicn.org service
      *
      * @return the air quality data object mapping the JSON response or null in case of error
      */
-    private GeoThunkJsonResponse getGeoThunkData() {
+    @Nullable
+    private GeoThunkJsonResponse getGeoThunkData(String urlStr) {
         GeoThunkJsonResponse result = null;
         String errorMsg = null;
 
-        String urlStr = buildRequestURL();
         logger.debug("URL = {}", urlStr);
 
         try {
@@ -220,30 +230,27 @@ public class GeoThunkHandler extends BaseThingHandler {
                 IOUtils.closeQuietly(connection.getInputStream());
             }
 
-            if (result == null) {
-                errorMsg = "Couldn't get a response from geothunk";
-            } else if (result.getHumidityPercent() <= 0) {
+            if (result.getHumidityPercent() <= 0) {
                 errorMsg = "Humidity returned 0%; is a wire loose?";
             } else {
-                String description = String.format("LastUpdate: %d; Humidity: %d; Temperature: %d; PM25: %d", result.getLastUpdate(), result.getHumidityPercent(), result.getTemperatureCelsius(), result.getPm2_5());
+                String description = String.format("LastUpdate: %d; Humidity: %d; Temperature: %d; PM25: %d",
+                        result.getLastUpdate(), result.getHumidityPercent(), result.getTemperatureCelsius(),
+                        result.getPm2_5());
                 updateStatus(ThingStatus.ONLINE, ThingStatusDetail.NONE, description);
                 return result;
             }
-            retryCounter++;
             logger.warn("Error in geothunk binding ({}); {}", urlStr, errorMsg);
-            if (retryCounter == 1) {
-                return getGeoThunkData();
-            } else {
-                logger.warn("request: {}", errorMsg);
-            }
         } catch (MalformedURLException e) {
             errorMsg = e.getMessage();
             logger.warn("Constructed url {} is not valid: {}", urlStr, errorMsg);
         } catch (JsonSyntaxException e) {
-            errorMsg = "Configuration is incorrect";
-            logger.warn("Error running geothunk binding (Air Quality) request: {}", errorMsg);
+            errorMsg = "Failed to parse response; " + e.getMessage();
+            logger.warn("Error running geothunk binding (Air Quality) request: {} {}", errorMsg, e.getMessage());
         } catch (IOException | IllegalStateException e) {
             errorMsg = e.getMessage();
+        } catch (Throwable e) {
+            errorMsg = "Some other exception occurred";
+            logger.error("Unhandled exception in GeoThunkResponse", e);
         }
 
         updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.OFFLINE.COMMUNICATION_ERROR, errorMsg);
